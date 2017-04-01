99d6b6acc1bd26eef09d8b85ba397b4f235567d0
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-01 09:54:02 -0700

    Set up TypeScript

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,1 +1,11 @@
-// Write your Javascript code.
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=script.js.map
