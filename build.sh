78ad96448e5932dffa9e91cb397d9afc697ddf2a
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-01 10:05:29 -0700

    Add comments to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+echo "*** BEGIN TYPESCRIPT TRANSPILE ***"
 tsc -p ./TypeScript/tsconfig.json --outDir ./wwwroot/js/
+echo "*** END TYPESCRIPT TRANSPILE ***"
+echo "*** BEGIN DOTNET BUILD ***"
 dotnet build DungeonGenerator.csproj
+echo "*** END DOTNET BUILD ***"
