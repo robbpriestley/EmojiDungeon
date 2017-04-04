c6a5a2158513554c2b92944c88b4af7e7c1216e9
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-03 18:36:47 -0700

    Getting basic AJAX working

--- Controllers/IndexController.cs
+++ Controllers/IndexController.cs
@@ -12,5 +12,35 @@ namespace DigitalWizardry.Dungeon.Controllers
 			model.CssNames = dungeon.CssNames();
     		return View(model);
         }
+
+		public IActionResult Sum(int firstNumber, int secondNumber)
+		{
+			return Content((firstNumber + secondNumber).ToString(), "text/plain");
+		}
+
+		public IActionResult DisplayObject()
+		{
+			Destination destination = new Destination("Tokyo", "Japan", 1);
+			return Json(destination);
+		}
+
+		public IActionResult DungeonView()
+		{
+			return View();
+		}
+    }
+
+	public class Destination
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+
+        public Destination(string city, string country, int id = 0)
+        {
+            City = city;
+            Country = country;
+            Id = id;
+        }
     }
 }
