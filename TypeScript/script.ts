34900becae849f562d7e324455b712fc01ea3aca
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-04 19:55:20 -0700

    Getting AJAX working with view and spinner

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,4 +1,32 @@
-function Start()
+"use strict";
+
+function Reset()
 {
-	alert("Hello");
+	var spinner = SpinnerSetup();
+	spinner.spin($('#grid')[0]);
+	
+	$.ajax({
+		type: 'GET',
+		dataType: 'html',
+		url: '/Index/DungeonView',
+		success: function (result) {
+			$('#grid').html(result);
+		}
+	});
+}
+
+function SpinnerSetup() : Spinner
+{
+	var opts = 
+	{
+		lines: 9,
+		length: 0,
+		width: 13,
+		radius: 21,
+		color: '#fff',
+		opacity: 0.25,
+		rotate: 33
+	}
+
+	return new Spinner(opts);
 }
