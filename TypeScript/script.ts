fd6926aabab7fb374d8546cef9be381d8867db4b
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-07 07:40:43 -0700

    Fix endless spinner and clear existing tiles

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -17,29 +17,38 @@ function Reset() : void
 		{
 			dungeon = [JSON.parse(result)];  // As this is a reset, assign the JSON to element 0 of the dungeon array.
 			PresentDungeonLevel(dungeon[0]);
+			spinner.stop();
 		}
 	});
 }
 
+// This function is going to iterate through each div in the grid and determine its location. It will then
+// strip it of any existing CSS classes. This will clear any dungeon level currently being presented. That
+// also removes the "native" classes the div needs ("tile" and it's location class). Those classes are 
+// added back on, as well as the tile name class that allows the div to display a graphical tile from the
+// tile sprite sheet.
 function PresentDungeonLevel(dungeonLevel : object) : void
 {
 	for (var x = 0; x <= 14; x++) 
 	{
 		for (let y = 0; y <= 14; y++)
 		{
-			let gridLocation : string = GridLocation(x, y);
-			console.log(gridLocation + ' ' + dungeonLevel[x][y].CssName);
-			// Bug: need to remove old ones first. How?
-			$(gridLocation).addClass(dungeonLevel[x][y].CssName);
+			let gridReference : string = GridReference(x, y);
+			let gridId : string = "#" + gridReference;       // Prepend the "#" because jQuery needs it for the id attribute.
+			$(gridId).removeClass();                         // Remove all classes on the div.
+			$(gridId).addClass("tile");                      // Add "native" class.
+			$(gridId).addClass(gridReference);               // Add "native" class.
+			$(gridId).addClass(dungeonLevel[x][y].CssName);  // Add tile name class.
 		}
 	}
 }
 
-function GridLocation(x : number, y : number) : string
+// Synthesize a grid reference given X and Y coords.
+function GridReference(x : number, y : number) : string
 {
 	let xs = x < 10 ? "0" + x.toString() : x.toString();
 	let ys = y < 10 ? "0" + y.toString() : y.toString();
-	return "#g" + xs + "-" + ys;  // Prepend the "#" because jQuery needs it for the id attribute.
+	return "g" + xs + "-" + ys;
 }
 
 // *** BEGIN UTILITY ***
