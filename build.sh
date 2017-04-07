9aafbee7dcbf6192ad63c48167b414cf058f65db
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-07 13:19:01 -0700

    Fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "*** BEGIN TYPESCRIPT TRANSPILE ***"
-tsc -p ./TypeScript/tsconfig.json --outDir ./wwwroot/js/
+tsc -p ./wwwroot/js/tsconfig.json --outDir ./wwwroot/js/
 echo "*** END TYPESCRIPT TRANSPILE ***"
 echo "*** BEGIN DOTNET BUILD ***"
 dotnet build DungeonGenerator.csproj
