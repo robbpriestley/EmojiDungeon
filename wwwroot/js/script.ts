8cde5240feaddeb4efd3b879d94aa6cdc7eb1c08
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-16 20:19:43 -0700

    Getting basic level up and down working

--- wwwroot/js/script.ts
+++ wwwroot/js/script.ts
@@ -1,25 +1,63 @@
 "use strict";
 
-let dungeon : object = null;
+window.onload = function(){Start();}
 
-function Reset() : void
+function Start() : void
 {
-	var spinner = SpinnerSetup();
+	sessionStorage.clear();
+	SetLevel(0);            // Start at level 0...
+	$("#level").text("1");  // ...presented to user as level 1.
+	RenderLevel(0);
+}
+
+function LevelUp() : void
+{
+	let level : number = Number(sessionStorage.getItem("level"));
+	level--  // Levels closer to the "surface" have smaller numbers.
+	SetLevel(level);
+	RenderLevel(level);
+	$("#level").text(level + 1);  // Add 1 for display as _level is zero-indexed.
+	sessionStorage.setItem("level", level.toString());
+}
+
+function LevelDown() : void
+{
+	let level : number = Number(sessionStorage.getItem("level"));
+	level++  // Levels farther from the "surface" have larger numbers.
+	SetLevel(level);
+	RenderLevel(level);
+	$("#level").text(level + 1);  // Add 1 for display as _level is zero-indexed.
+	sessionStorage.setItem("level", level.toString());
+}
+
+function RenderLevel(level : number)
+{
+	let spinner : Spinner = SpinnerSetup();
 	spinner.spin($('#grid')[0]);
+
+	let dungeon : object = GetDungeon(level);
 	
-	$.ajax
-	({
-		type: 'GET',
-		dataType: 'json',
-		contentType: 'application/json',
-		url: '/Index/DungeonView',
-		success: function (result) 
-		{
-			dungeon = [JSON.parse(result)];  // As this is a reset, assign the JSON to element 0 of the dungeon array.
-			PresentDungeonLevel(dungeon[0]);
-			spinner.stop();
-		}
-	});
+	if (dungeon != null)
+	{
+		BuildGrid(level, dungeon);
+	}
+	else
+	{
+		$.ajax
+		({
+			type: 'GET',
+			dataType: 'json',
+			contentType: 'application/json',
+			url: '/Index/DungeonView',
+			success: function (result) 
+			{
+				dungeon = JSON.parse(result);  // As this is a reset, assign the JSON to element 0 of the dungeon array.
+				BuildGrid(level, dungeon);
+				SetDungeon(level, dungeon);
+				spinner.stop();
+			}
+		});
+	}
 }
 
 // This function is going to iterate through each div in the grid and determine its location. It will then
@@ -27,7 +65,7 @@ function Reset() : void
 // also removes the "native" classes the div needs ("tile" and it's location class). Those classes are 
 // added back on, as well as the tile name class that allows the div to display a graphical tile from the
 // tile sprite sheet.
-function PresentDungeonLevel(dungeonLevel : object) : void
+function BuildGrid(level : number, dungeon : object) : void
 {
 	for (var x = 0; x <= 14; x++) 
 	{
@@ -38,7 +76,7 @@ function PresentDungeonLevel(dungeonLevel : object) : void
 			$(gridId).removeClass();                         // Remove all classes on the div.
 			$(gridId).addClass("tile");                      // Add "native" class.
 			$(gridId).addClass(gridReference);               // Add "native" class.
-			$(gridId).addClass(dungeonLevel[x][y].CssName);  // Add tile name class.
+			$(gridId).addClass(dungeon[x][y].CssName);  // Add tile name class.
 		}
 	}
 }
@@ -51,6 +89,38 @@ function GridReference(x : number, y : number) : string
 	return "g" + xs + "-" + ys;
 }
 
+// *** BEGIN ACCESSORS ***
+
+function GetLevel() : number
+{
+	return Number(sessionStorage.getItem("level"));
+}
+
+function SetLevel(level : number) : void
+{
+	sessionStorage.setItem("level", level.toString());
+}
+
+function GetDungeon(level : number) : object
+{
+	if (sessionStorage.getItem("dungeon" + level.toString()) === null)
+	{
+		return null;
+	}
+	else
+	{
+		// Leave the cast to Array<object> alone. It seems to be important in the LevelUp() scenario.
+		let dungeon : object = Array<object>(JSON.parse(sessionStorage.getItem("dungeon" + level.toString())));
+		return dungeon[0];
+	}
+}
+
+function SetDungeon(level : number, dungeon: object) : void
+{
+	sessionStorage.setItem("dungeon" + level.toString(), JSON.stringify(dungeon));
+}
+
+// *** END ACCESSORS ***
 // *** BEGIN UTILITY ***
 
 function SpinnerSetup() : Spinner
