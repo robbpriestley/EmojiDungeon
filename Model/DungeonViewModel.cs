097c3efca17a2774312b1efbdf5494dd897b0aa9
Robb Priestley <wizard@Robbs-MacBook.hitronhub.home>
2017-04-24 07:25:31 -0700

    Fix comments for accuracy

--- Model/DungeonViewModel.cs
+++ Model/DungeonViewModel.cs
@@ -3,8 +3,8 @@ namespace DigitalWizardry.Dungeon.Models
 	// Each one of these is equivalent to a dungeon Cell.
 	public class DungeonViewModelCell
 	{
-		public string D { get; set; }  // Door direction list, ex: U: door up, UR: doors up and right. Or, null if no doors.
-		public string K { get; set; }  // Key exists at this cell location, 1: true, 0: false.
+		public string D { get; set; }  // Door direction: "U", "D", "L", "R", or "" if no door in the cell.
+		public string K { get; set; }  // Key exists at this cell location, "K": key, "": no key.
 		public string N { get; set; }  // CssName
 		public string L { get; set; }  // CssLocation
 	}
