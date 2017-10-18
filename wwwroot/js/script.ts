452f1e9f66fb6e8c33996cfacbd1e94fb4daf833
Robb Priestley <robb@digitalwizardry.ca>
2017-10-18 14:23:46 -0700

    State is no longer stored in sessionStorage, it is stored in global variables.

--- wwwroot/js/script.ts
+++ wwwroot/js/script.ts
@@ -1,72 +1,71 @@
 "use strict";
 
+let Level: number;
+let PlayerCoords: Coords;
+let Dungeon: Array<object> = new Array<object>();
+let StartCoords: Array<Coords> = new Array<Coords>();
+
 window.onload = function(){Start();}
 
-function Start() : void
+function Start(): void
 {
 	sessionStorage.clear();
-	SetLevel(1);
+	Level = 1;
 	$("#level").text("1");
-	RenderLevel(1);
+	RenderLevel();
 }
 
-function LevelUp() : void
+function LevelUp(): void
 {
-	let level : number = Number(sessionStorage.getItem("level"));
-	level--  // Levels closer to the "surface" have smaller numbers.
-	SetLevel(level);
-	RenderLevel(level);
-	$("#level").text(level);  // Add 1 for display as _level is zero-indexed.
-	sessionStorage.setItem("level", level.toString());
+	Level--;  // Levels closer to the "surface" have smaller numbers.
+	RenderLevel();
+	$("#level").text(Level);
 }
 
-function LevelDown() : void
+function LevelDown(): void
 {
-	let level : number = Number(sessionStorage.getItem("level"));
-	level++  // Levels farther from the "surface" have larger numbers.
-	SetLevel(level);
-	RenderLevel(level);
-	$("#level").text(level);  // Add 1 for display as _level is zero-indexed.
-	sessionStorage.setItem("level", level.toString());
+	Level++;
+	RenderLevel();
+	$("#level").text(Level);  // Add 1 for display as _level is zero-indexed.
 }
 
-function RenderLevel(level : number)
+function RenderLevel()
 {
-	let dungeon : object = GetDungeon(level);
+	let dungeon: object = Dungeon[Level];
 	
 	if (dungeon != null)
 	{
-		BuildDungeon(level, dungeon);
+		BuildDungeon(Level, dungeon);
 	}
 	else
 	{
-		let spinner : Spinner = SpinnerSetup();
+		let spinner: Spinner = SpinnerSetup();
 		spinner.spin($('#grid')[0]);
 
-		let startCoords : string = GetStartCoords(level);
-		let sX : number = level == 1 ? 7 : Number(startCoords.substring(1, 3));
-		let sY : number = level == 1 ? 0 : Number(startCoords.substring(3, 5));
-		let sD : string = level == 1 ? "U" : startCoords.substring(5, 6);
+		let startCoords: Coords = StartCoords[Level];
+		let sX: number = Level == 1 ? 7: Number(startCoords.X);
+		let sY: number = Level == 1 ? 0: Number(startCoords.Y);
+		let sD: string = Level == 1 ? "U": startCoords.Dir;
 		
 		$.ajax
 		({
 			type: 'GET',
 			dataType: 'json',
-			data: { level: level, startX: sX, startY: sY, direction: sD },
+			data: { level: Level, startX: sX, startY: sY, direction: sD },
 			contentType: 'application/json',
 			url: '/Index/DungeonView',
 			success: function (result) 
 			{
 				dungeon = JSON.parse(result);  // As this is a reset, assign the JSON to element 0 of the dungeon array.
-				BuildDungeon(level, dungeon);
-				SetDungeon(level, dungeon);
+				BuildDungeon(Level, dungeon);
+				Dungeon[Level] = dungeon;
 				spinner.stop();
 			}
 		});
 	}
 }
 
-function SpinnerSetup() : Spinner
+function SpinnerSetup(): Spinner
 {
 	var opts = 
 	{
@@ -87,7 +86,7 @@ function SpinnerSetup() : Spinner
 // also removes the "native" classes the div needs ("tile" and it's location class). Those classes are 
 // added back on, as well as the tile name class that allows the div to display a graphical tile from the
 // tile sprite sheet.
-function BuildDungeon(level : number, dungeon : object) : void
+function BuildDungeon(level: number, dungeon: object): void
 {
 	RemoveSprites();
 	
@@ -95,18 +94,18 @@ function BuildDungeon(level : number, dungeon : object) : void
 	{
 		for (let y = 0; y <= 14; y++)
 		{
-			let gridReference : string = GridReference(x, y);
-			let gridId : string = "#" + gridReference;       // Prepend the "#" because jQuery needs it for the id attribute.
+			let gridReference: string = GridReference(x, y);
+			let gridId: string = "#" + gridReference;       // Prepend the "#" because jQuery needs it for the id attribute.
 			$(gridId).removeClass();                         // Remove all classes on the div.
 			$(gridId).addClass("tile");                      // Add "native" class.
 			$(gridId).addClass(gridReference);               // Add "native" class.
 			
-			let tileName : string = dungeon[x][y].N
+			let tileName: string = dungeon[x][y].N
 			$(gridId).addClass(tileName);                    // Add tile name class.
 
 			if (tileName == "e6" || tileName == "e7" || tileName == "e8" || tileName == "e9")
 			{
-				RecordStart(level, tileName, gridReference);
+				RecordStart(level, tileName, new Coords(x, y));
 			}
 
 			PlaceAnyGoblin(x, y, dungeon[x][y].X);
@@ -118,7 +117,7 @@ function BuildDungeon(level : number, dungeon : object) : void
 }
 
 // Remove any sprites that are "local" to a different dungeon level.
-function RemoveSprites() : void
+function RemoveSprites(): void
 {
 	$('.player').each
 	(
@@ -186,71 +185,68 @@ function RemoveSprites() : void
 }
 
 // Synthesize a grid reference given X and Y coords.
-function GridReference(x : number, y : number) : string
+function GridReference(x: number, y: number): string
 {
-	let xs = x < 10 ? "0" + x.toString() : x.toString();
-	let ys = y < 10 ? "0" + y.toString() : y.toString();
+	let xs = x < 10 ? "0" + x.toString(): x.toString();
+	let ys = y < 10 ? "0" + y.toString(): y.toString();
 	return "g" + xs + ys;
 }
 
-function RecordStart(level : number, tileName : string, gridReference : string) : void
+function RecordStart(level: number, tileName: string, coords: Coords): void
 {
-	let direction : string = null;
+	let direction: string = null;
 
 	switch (tileName)
 	{
 		case "e6":
-			direction = "U";
+			coords.Dir = "U";
 			break;
 
 		case "e7":
-			direction = "D";
+			coords.Dir = "D";
 			break;
 
 		case "e8":
-			direction = "L";
+			coords.Dir = "L";
 			break;
 
 		case "e9":
-			direction = "R";
+			coords.Dir = "R";
 			break;
 	
 		default:
 			break;
 	}
 
-	SetStartCoords(level + 1, gridReference + direction);
+	StartCoords[Level + 1] = coords;
 }
 
 // *** BEGIN PLAYER ***
 
-function PlacePlayer() : void
-{
-	let level : number = GetLevel();
-	
-	let x : number = 7;
-	let y : number = 0;
+function PlacePlayer(): void
+{	
+	let x: number = 7;
+	let y: number = 0;
 
-	if (level != 1)
+	if (Level != 1)
 	{
-		let startCoords : string = GetStartCoords(level);
-		x = Number(startCoords.substring(1, 3));
-		y = Number(startCoords.substring(3, 5));
+		let startCoords: Coords = StartCoords[Level];
+		x = Number(startCoords.X);
+		y = Number(startCoords.Y);
 	}
 	
-	let coords : Coords = new Coords(x, y);
-	SetPlayerCoords(coords);
+	PlayerCoords = new Coords(x, y);
 
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div id="player" class="sprite player" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
-function MovePlayer(coords : Coords) : void
+function MovePlayer(coords: Coords): void
 {
-	SetPlayerCoords(coords);
-	let xPixels : number = coords.X * 45;
-	let yPixels : number = 630 - (coords.Y * 45);
+	PlayerCoords = coords;
+	let xPixels: number = coords.X * 45;
+	let yPixels: number = 630 - (coords.Y * 45);
 	$("#player").css("top", yPixels + "px");
 	$("#player").css("left", xPixels + "px");
 }
@@ -258,7 +254,7 @@ function MovePlayer(coords : Coords) : void
 // *** END PLAYER ***
 // *** BEGIN GOBLINS ***
 
-function PlaceAnyGoblin(x : number, y : number, X : string) : void
+function PlaceAnyGoblin(x: number, y: number, X: string): void
 {
 	if (X == "G")
 	{
@@ -270,24 +266,24 @@ function PlaceAnyGoblin(x : number, y : number, X : string) : void
 	}
 }
 
-function PlaceGoblin(x : number, y : number) : void
+function PlaceGoblin(x: number, y: number): void
 {
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div class="sprite goblingreen" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
-function PlaceBlackGoblin(x : number, y : number) : void
+function PlaceBlackGoblin(x: number, y: number): void
 {
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div class="sprite goblinblack" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
 // *** END GOBLINS ***
 // *** BEGIN DOORS, KEYS, GEMS, and HEARTS ***
 
-function PlaceAnyItem(x : number, y : number, D : string, K : string, G : string, H : string) : void
+function PlaceAnyItem(x: number, y: number, D: string, K: string, G: string, H: string): void
 {
 	if (D != "")
 	{
@@ -307,17 +303,17 @@ function PlaceAnyItem(x : number, y : number, D : string, K : string, G : string
 	}
 }
 
-function PlaceDoor(x : number, y : number, direction : string) : void
+function PlaceDoor(x: number, y: number, direction: string): void
 {
-	let xPixels : number = x * 45 + DoorXFudge(direction);
-	let yPixels : number = 630 - (y * 45) + DoorYFudge(direction);
-	let doorClass : string = direction == "U" || direction == "D" ? "doorh" : "doorv";
+	let xPixels: number = x * 45 + DoorXFudge(direction);
+	let yPixels: number = 630 - (y * 45) + DoorYFudge(direction);
+	let doorClass: string = direction == "U" || direction == "D" ? "doorh": "doorv";
 	$("#grid").append('<div class="sprite ' + doorClass + '" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
-function DoorXFudge(direction : string) : number
+function DoorXFudge(direction: string): number
 {
-	let fudge : number = 0;
+	let fudge: number = 0;
 
 	switch (direction)
 	{
@@ -344,9 +340,9 @@ function DoorXFudge(direction : string) : number
 	return fudge;
 }
 
-function DoorYFudge(direction : string) : number
+function DoorYFudge(direction: string): number
 {
-	let fudge : number = 0;
+	let fudge: number = 0;
 
 	switch (direction)
 	{
@@ -373,87 +369,28 @@ function DoorYFudge(direction : string) : number
 	return fudge;
 }
 
-function PlaceKey(x : number, y : number) : void
+function PlaceKey(x: number, y: number): void
 {
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div class="sprite key" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
-function PlaceGem(x : number, y : number) : void
+function PlaceGem(x: number, y: number): void
 {
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div class="sprite gem" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
-function PlaceHeart(x : number, y : number) : void
+function PlaceHeart(x: number, y: number): void
 {
-	let xPixels : number = x * 45;
-	let yPixels : number = 630 - (y * 45);
+	let xPixels: number = x * 45;
+	let yPixels: number = 630 - (y * 45);
 	$("#grid").append('<div class="sprite heartr" style="top: ' + yPixels + 'px; left: ' + xPixels + 'px;"></div>');
 }
 
 // *** END DOORS, KEYS, GEMS, and HEARTS ***
-// *** BEGIN ACCESSORS ***
-
-function GetLevel() : number
-{
-	return Number(sessionStorage.getItem("level"));
-}
-
-function SetLevel(level : number) : void
-{
-	sessionStorage.setItem("level", level.toString());
-}
-
-function GetStartCoords(level : number) : string
-{
-	return sessionStorage.getItem("start" + level.toString());
-}
-
-function SetStartCoords(level : number, coords : string) : void
-{
-	sessionStorage.setItem("start" + level.toString(), coords);
-}
-
-function GetPlayerCoords() : Coords
-{
-	let rawCoords = sessionStorage.getItem("playerCoords");
-	let x : number = Number(rawCoords.substring(0, 2));
-	let y : number = Number(rawCoords.substring(2, 4));
-	let coords : Coords = new Coords(x, y);
-	return coords;
-}
-
-function SetPlayerCoords(coords : Coords) : void
-{
-	let xPart : string = coords.X < 10 ? "0" + coords.X.toString() : coords.X.toString();
-	let yPart : string = coords.Y < 10 ? "0" + coords.Y.toString() : coords.Y.toString();
-	let coordsString : string = xPart + yPart;
-	sessionStorage.setItem("playerCoords", coordsString);
-}
-
-function GetDungeon(level : number) : object
-{
-	if (sessionStorage.getItem("dungeon" + level.toString()) === null)
-	{
-		return null;
-	}
-	else
-	{
-		// Leave the cast to Array<object> alone. It seems to be important in the LevelUp() scenario.
-		let dungeon : object = Array<object>(JSON.parse(sessionStorage.getItem("dungeon" + level.toString())));
-		return dungeon[0];
-	}
-}
-
-function SetDungeon(level : number, dungeon: object) : void
-{
-	sessionStorage.setItem("dungeon" + level.toString(), JSON.stringify(dungeon));
-}
-
-// *** END ACCESSORS ***
 // *** BEGIN USER INPUT ***
 
 document.onkeydown = KeyPress;
@@ -483,29 +420,27 @@ function KeyPress(e)
 // *** END USER INPUT ***
 // *** BEGIN MOVEMENT ***
 
-function PlayerMove(dir : string)
-{
-	let coords : Coords = GetPlayerCoords();
-	
-	if (dir == "U" && coords.Y < 14)
+function PlayerMove(dir: string)
+{	
+	if (dir == "U" && PlayerCoords.Y < 14)
 	{
-		coords.Y += 1;
-		MovePlayer(coords);
+		PlayerCoords.Y += 1;
+		MovePlayer(PlayerCoords);
 	}
-	else if (dir == "D" && coords.Y > 0)
+	else if (dir == "D" && PlayerCoords.Y > 0)
 	{
-		coords.Y -= 1;
-		MovePlayer(coords);
+		PlayerCoords.Y -= 1;
+		MovePlayer(PlayerCoords);
 	}
-	else if (dir == "L" && coords.X > 0)
+	else if (dir == "L" && PlayerCoords.X > 0)
 	{
-		coords.X -= 1;
-		MovePlayer(coords);
+		PlayerCoords.X -= 1;
+		MovePlayer(PlayerCoords);
 	}
-	else if (dir == "R" && coords.X < 14)
+	else if (dir == "R" && PlayerCoords.X < 14)
 	{
-		coords.X += 1;
-		MovePlayer(coords);
+		PlayerCoords.X += 1;
+		MovePlayer(PlayerCoords);
 	}
 	else
 	{
@@ -518,10 +453,11 @@ function PlayerMove(dir : string)
 
 class Coords
 {
-    X : number;
-    Y : number;
+    X: number;
+	Y: number;
+	Dir: string;
 
-	constructor(x : number, y : number) 
+	constructor(x: number, y: number) 
 	{
         this.X = x;
 		this.Y = y;
